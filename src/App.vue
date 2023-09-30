7a9041218a856619eba134754376d4d21f0b55f2
tabby-02 <1753779141@qq.com>
2023-09-30 17:05:04 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="tabby-02" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
